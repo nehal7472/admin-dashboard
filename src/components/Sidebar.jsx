e3d64505678a27145c0d776bd0c06e85dc8ecf56
Ahsanul Haque Nehal <007cryptic@gmail.com>
2025-06-17 23:24:34 +0600

    add some new things

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,15 +13,23 @@ const Sidebar = () => {
 
   return (
     <aside className="w-full md:w-64 bg-black text-white px-10 py-12 flex flex-col gap-16 justify-center items-center md:justify-start md:items-start">
-      <div className="flex flex-col items-center md:items-start ">
-        <img
-          src="profile.png"
-          alt="Profile"
-          className="rounded-full w-16 h-16 mb-2"
-        />
-        <h2 className="font-bold text-lg">Ahsanul Haque</h2>
-        <p className="text-sm">007cryptic@gmail.com</p>
+      <div className="flex flex-col items-center md:items-start text-white">
+        <div className="relative">
+          <img
+            src="profile.png"
+            alt="Profile"
+            className="rounded-xl w-16 h-16 mb-2"
+          />
+
+          <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full">
+            4
+          </span>
+        </div>
+
+        <h2 className="font-bold text-lg mt-1">Ahsanul Haque nehal</h2>
+        <p className="text-sm text-gray-400">007cryptic@gmail.com</p>
       </div>
+
       <Header navItems={navItems} />
     </aside>
   );
